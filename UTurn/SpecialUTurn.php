9d6dab21979301a7b1b02fda65ad48051ab861cd
Peter Hankiewicz <peterh@cyber.harvard.edu>
2020-07-31 14:11:17 -0400

    Actually remove pages and related entities

--- UTurn/SpecialUTurn.php
+++ UTurn/SpecialUTurn.php
@@ -415,7 +415,7 @@ class SpecialUTurn extends SpecialPage {
                         if ( $deletePages && array_key_exists( 'delete', $theRevision ) ) {
                             $errors = array();
                             // doDeleteArticleReal was not defined until 1.19, this will need to be revised when 1.18 is less prevalent
-                            $currentPage->doDeleteArticleReal( $summary, false, 0, true, $errors, User::newFromSession() );
+                            $this->deletePermanently($currentPage->mTitle);
 
                             if ($namespace == NS_FILE){
                                 $file = wfFindFile($currentPage->mTitle, array( 'ignoreRedirect' => true ) );
@@ -439,4 +439,180 @@ class SpecialUTurn extends SpecialPage {
             } 
         }
     }
+
+    private function deletePermanently( Title $title ) {
+      $ns = $title->getNamespace();
+      $t = $title->getDBkey();
+      $id = $title->getArticleID();
+      $cats = $title->getParentCategories();
+
+      $dbw = wfGetDB( DB_MASTER );
+
+      $dbw->startAtomic( __METHOD__ );
+
+      /*
+       * First delete entries, which are in direct relation with the page:
+       */
+
+      # Delete redirect...
+      $dbw->delete( 'redirect', [ 'rd_from' => $id ], __METHOD__ );
+
+      # Delete external links...
+      $dbw->delete( 'externallinks', [ 'el_from' => $id ], __METHOD__ );
+
+      # Delete language links...
+      $dbw->delete( 'langlinks', [ 'll_from' => $id ], __METHOD__ );
+
+      if ( $GLOBALS['wgDBtype'] !== "postgres" && $GLOBALS['wgDBtype'] !== "sqlite" ) {
+        # Delete search index...
+        $dbw->delete( 'searchindex', [ 'si_page' => $id ], __METHOD__ );
+      }
+
+      # Delete restrictions for the page
+      $dbw->delete( 'page_restrictions', [ 'pr_page' => $id ], __METHOD__ );
+
+      # Delete page links
+      $dbw->delete( 'pagelinks', [ 'pl_from' => $id ], __METHOD__ );
+
+      # Delete category links
+      $dbw->delete( 'categorylinks', [ 'cl_from' => $id ], __METHOD__ );
+
+      # Delete template links
+      $dbw->delete( 'templatelinks', [ 'tl_from' => $id ], __METHOD__ );
+
+      # Read text entries for all revisions and delete them.
+      $res = $dbw->select( 'revision', 'rev_text_id', "rev_page=$id" );
+
+      foreach ( $res as $row ) {
+        $value = $row->rev_text_id;
+        $dbw->delete( 'text', [ 'old_id' => $value ], __METHOD__ );
+      }
+
+      # In the table 'revision' : Delete all the revision of the page where 'rev_page' = $id
+      $dbw->delete( 'revision', [ 'rev_page' => $id ], __METHOD__ );
+
+      # Delete image links
+      $dbw->delete( 'imagelinks', [ 'il_from' => $id ], __METHOD__ );
+
+      /*
+       * then delete entries which are not in direct relation with the page:
+       */
+
+      # Clean up recentchanges entries...
+      $dbw->delete( 'recentchanges', [
+        'rc_namespace' => $ns,
+        'rc_title' => $t
+      ], __METHOD__ );
+
+      # Read text entries for all archived pages and delete them.
+      $res = $dbw->select( 'archive', 'ar_text_id', [
+        'ar_namespace' => $ns,
+        'ar_title' => $t
+      ] );
+
+      foreach ( $res as $row ) {
+        $value = $row->ar_text_id;
+        $dbw->delete( 'text', [ 'old_id' => $value ], __METHOD__ );
+      }
+
+      # Clean up archive entries...
+      $dbw->delete( 'archive', [
+        'ar_namespace' => $ns,
+        'ar_title' => $t
+      ], __METHOD__ );
+
+      # Clean up log entries...
+      $dbw->delete( 'logging', [
+        'log_namespace' => $ns,
+        'log_title' => $t
+      ], __METHOD__ );
+
+      # Clean up watchlist...
+      $dbw->delete( 'watchlist', [
+        'wl_namespace' => $ns,
+        'wl_title' => $t
+      ], __METHOD__ );
+
+      $dbw->delete( 'watchlist', [
+        'wl_namespace' => MWNamespace::getAssociated( $ns ),
+        'wl_title' => $t
+      ], __METHOD__ );
+
+      # In the table 'page' : Delete the page entry
+      $dbw->delete( 'page', [ 'page_id' => $id ], __METHOD__ );
+
+      /*
+       * If the article belongs to a category, update category counts
+       */
+      if ( !empty( $cats ) ) {
+        foreach ( $cats as $parentcat => $currentarticle ) {
+          $catname = preg_split( '/:/', $parentcat, 2 );
+          $cat = Category::newFromName( $catname[1] );
+          if ( !is_object( $cat ) ) {
+            // Blank error to allow us to continue
+          } else {
+            $cat->refreshCounts();
+          }
+        }
+      }
+
+      /*
+       * If an image is being deleted, some extra work needs to be done
+       */
+      if ( $ns == NS_FILE ) {
+        if ( method_exists( MediaWikiServices::class, 'getRepoGroup' ) ) {
+          // MediaWiki 1.34+
+          $file = MediaWikiServices::getInstance()->getRepoGroup()->findFile( $t );
+        } else {
+          $file = wfFindFile( $t );
+        }
+
+        if ( $file ) {
+          # Get all filenames of old versions:
+          $fields = OldLocalFile::selectFields();
+          $res = $dbw->select( 'oldimage', $fields, [ 'oi_name' => $t ] );
+
+          foreach ( $res as $row ) {
+            $oldLocalFile = OldLocalFile::newFromRow( $row, $file->repo );
+            $path = $oldLocalFile->getArchivePath() . '/' . $oldLocalFile->getArchiveName();
+
+            try {
+              unlink( $path );
+            }
+            catch ( Exception $e ) {
+              return $e->getMessage();
+            }
+          }
+
+          $path = $file->getLocalRefPath();
+
+          try {
+            $file->purgeThumbnails();
+            unlink( $path );
+          } catch ( Exception $e ) {
+            return $e->getMessage();
+          }
+        }
+
+        # Clean the filearchive for the given filename:
+        $dbw->delete( 'filearchive', [ 'fa_name' => $t ], __METHOD__ );
+
+        # Delete old db entries of the image:
+        $dbw->delete( 'oldimage', [ 'oi_name' => $t ], __METHOD__ );
+
+        # Delete archive entries of the image:
+        $dbw->delete( 'filearchive', [ 'fa_name' => $t ], __METHOD__ );
+
+        # Delete image entry:
+        $dbw->delete( 'image', [ 'img_name' => $t ], __METHOD__ );
+
+        // $dbw->endAtomic( __METHOD__ );
+
+        $linkCache = MediaWikiServices::getInstance()->getLinkCache();
+        $linkCache->clear();
+      }
+      $dbw->endAtomic( __METHOD__ );
+      return true;
+    }
 }
+
